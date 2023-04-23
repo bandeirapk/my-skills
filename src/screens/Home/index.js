6ca97c888a0beed37c855a1e6d0c6a796e295661
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2023-04-23 20:29:49 -0300

    style: estilização na página home
    Foi adicionado alguns componenets visuais na interface home para melhorar a aparência do app.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { View, Text, TextInput, FlatList } from 'react-native';
 
@@ -10,14 +10,28 @@ import { styles } from './styles';
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
 
   function handleAddSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGreeting('Good Morning ☀️');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good Afternoon ☀️');
+    } else {
+      setGreeting('Good Night 🌑');
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome Bandeira</Text>
+      <Text style={styles.greeting}>{greeting}</Text>
 
       <TextInput
         style={styles.input}
@@ -30,7 +44,18 @@ export function Home() {
 
       <Button onPress={handleAddSkill} />
 
-      <Text style={[styles.title, { marginVertical: 50 }]}> My Skills </Text>
+      <View style={styles.containerCounterSkills}>
+        <Text
+          style={[
+            styles.title,
+            { backgroundColor: '#A370F7', borderRadius: 14, padding: 2 },
+          ]}
+        >
+          {' '}
+          My Skills{' '}
+        </Text>
+        <Text style={styles.counterSkills}> {mySkills.length} </Text>
+      </View>
 
       <FlatList
         data={mySkills}
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -14,6 +14,11 @@ export const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#FFF',
   },
+  greeting: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFF',
+  },
   input: {
     marginTop: Platform.OS === 'ios' ? 15 : 20,
 
@@ -25,5 +30,26 @@ export const styles = StyleSheet.create({
     fontSize: 18,
 
     backgroundColor: '#1F1E25',
+  },
+  containerCounterSkills: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    borderBottomWidth: 4,
+    borderBottomColor: '#FFF',
+
+    paddingBottom: 20,
+
+    marginTop: 50,
+  },
+  counterSkills: {
+    borderRadius: 14,
+
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFF',
+
+    backgroundColor: '#A370F7',
   },
 });
